cc9891c0f380e659ac03c9d389a96dae263c08fb
unknown <prakashbahadursingh2@gmail.com>
2020-04-09 00:18:27 -0700

    keydown eventlistener complete

--- js/index.js
+++ js/index.js
@@ -4,13 +4,16 @@
 const get = (selector) => {
     return document.querySelector(selector)
   }
+// const get = function(selector){
+//     document.querySelector(selector);
+// }
   
 //selecting elements using using get (CSS selector) function
 
 // navigation bar
 const logoHeading = get('.logo-heading')
-const nalLink = get('.nav-link')
-
+const navLink = get('.nav-link')
+const nav = get('nav')
 // container home
 const welcomeToFunBus = get ('.home .intro h2')
 const letsGo = get('.home .content-section .text-section h2')
@@ -23,17 +26,80 @@ const secondSlideImg = get('.home .content-destination img')
 const button = get('.home .destination .btn')
 
 // creating event listeners
-function lunch(event){
+
+// mouseover**********************
+function mouseoverFunBush(event){
+    
 event.target.style.backgroundColor = 'gold';
 event.target.style.color = 'red'
 setTimeout(function(){
     event.target.style.backgroundColor ='';
-}, 500)
+}, 200)
 setTimeout(function(){
     event.target.style.color ='';
-}, 500)
+}, 200)
 
-} false;
-logoHeading.addEventListener('mouseover', lunch)
+};
+logoHeading.addEventListener('mouseover', mouseoverFunBush)
 
 
+// keydown**************
+function navBackColor (event){
+    if(event.key === 'Escape'){
+    nav.style.backgroundColor = 'orange' 
+    }
+}
+document.addEventListener('keydown', navBackColor)
+// document.addEventListener('keydown', event => {
+//     if(event.keyCode === '13'){
+//         alert('Hello!!! Welcome to the Fun Bus.....') 
+//     }
+//   });
+
+// wheel*******************s**
+
+document.addEventListener('wheel', event => {
+    event.preventDefault();
+    const delta = Math.sign(event.deltaY);
+    console.info(delta);
+})
+
+
+
+
+
+// function wheelLetsGoImg(event){
+  
+// }
+// letsGoImg.addEventListener('wheel', wheelLetsGoImg)
+
+
+// drag / drop*****************
+// const destinationDrag = document.querySelector('destination')
+// destinationDrag.setAttribute('draggable', 'true')
+// destinationDrag.appendChild()
+
+
+
+// load*****************
+
+
+// focus*****************
+
+
+// resize*****************
+
+
+// scroll*****************
+
+
+// select*****************
+
+
+// dblclick*****************
+// function dblclickNavLink(event){
+    
+//     event.target.style.backgroundColor = 'orange'
+//     }
+    
+    // navLink.addEventListener('dblclick', addEventListener)
